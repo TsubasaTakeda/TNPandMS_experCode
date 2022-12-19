78e08d276f49becc1d8873ec02e91c034c7b494a
TsubasaTakeda <80091069+TsubasaTakeda@users.noreply.github.com>
2022-12-19 19:27:12 +0900

    FISTAを修正

--- TNPandMS_lib/optimizationProgram/accelGradient.py
+++ TNPandMS_lib/optimizationProgram/accelGradient.py
@@ -1,17 +1,70 @@
 
 import numpy as np
+import time
 from sqlalchemy import null
 
-class AccelGradient:
 
+class FISTA:
+
+    # 初期設定
     def __init__(self):
+        # 結果格納用
         self.iter = null
         self.sol = null
         self.sol_obj = null
         self.num_call_nbl = null
+        self.time = null
+        # 演算等に必要なデータ
+        self.obj_func = null
+        self.nbl_func = null
+        self.lips = null
+        self.x_init = null
+        self.conv_judge = null
+        self.output_iter = 1000
+
+    # 目的関数と勾配関数を設定(関数の引数はベクトルのみ)
+    def set_obj_func(self, obj_func):
+        self.obj_func = obj_func
+    def set_nbl_func(self, nbl_func):
+        self.nbl_func = nbl_func
+
+    # 計算に必要な各種設定をsetting
+    def set_lips(self, lips):
+        self.lips = lips
+    def set_output_iter(self, iter):
+        self.output_iter = iter
+    def set_conv_judge(self, conv):
+        self.conv_judge = conv
+    def set_x_init(self, x_init):
+        self.x_init = x_init
 
     # FISTA(初期解, 勾配関数, 勾配のリプシッツ定数, 収束判定値(勾配要素の絶対値の最大値), 結果出力の頻度)
-    def exect_FISTA(self, x_init, nbl_func, lip_cont, conv_judge, output_iter):
+    def exect_FISTA(self):
+
+        is_Executed = 0
+
+        print("\n\n")
+
+        if self.x_init is null:
+            print("init sol is not set.")
+            is_Executed = -1
+        if self.nbl_func is null:
+            print("nabla function is not set.")
+            is_Executed = -1
+        if self.lips is null:
+            print("lipschitz constant is not set.")
+            is_Executed = -1
+        if self.conv_judge is null:
+            print("convergence judgement (>0) is not set.")
+            is_Executed = -1
+
+        if is_Executed:
+            print("FISTA cannot be executed.\n\n")
+            return -1
+        
+        print('start Accel Gradient Method (FISTA)!')
+
+        start_time = time.process_time()
 
         # 初期設定（反復回数・勾配関数の呼び出し回数を初期化）
         iteration = 0
@@ -21,10 +74,9 @@ class AccelGradient:
         t = 1
 
         # 初期解の設定
-        now_sol = x_init
+        now_sol = self.x_init
         temp_sol = now_sol
 
-        print('start accel gradient method!')
         if len(now_sol) > 5:
             print('iteration:', iteration, ' now_sol:', now_sol[:5])
         else:
@@ -35,40 +87,216 @@ class AccelGradient:
             iteration += 1
 
             # 勾配計算（一時的な解の）
-            temp_nbl = nbl_func(temp_sol)
+            temp_nbl = self.nbl_func(temp_sol)
             num_call_nbl += 1
 
             # 暫定解の更新
             prev_sol = now_sol
-            now_sol = temp_sol - temp_nbl/lip_cont
+            now_sol = temp_sol - temp_nbl/self.lips
             temp_t = (1 + (1 + 4*t**2)**(1/2))/2
             temp_sol = now_sol + ((t - 1)/temp_t) * (now_sol - prev_sol)
             t = temp_t
 
             # 収束判定の準備
-            now_nbl = nbl_func(now_sol)
+            now_nbl = self.nbl_func(now_sol)
             num_call_nbl += 1
             nbl_max = np.max(now_nbl)
             nbl_min = np.min(now_nbl)
 
-            if iteration % output_iter == 0:
+            if iteration % self.output_iter == 0:
                 if len(now_sol) > 5:
                     print('iteration:', iteration, ' now_sol:', now_sol[:5], ' max_nabla:', nbl_max, ' min_nabla:', nbl_min)
                 else:
                     print('iteration:', iteration, ' now_sol:', now_sol, ' max_nabla:', nbl_max, ' min_nabla:', nbl_min)
 
             # 収束判定
-            if nbl_max < conv_judge and -nbl_min < conv_judge:
+            if nbl_max < self.conv_judge and -nbl_min < self.conv_judge:
                 break
+
+        end_time = time.process_time()
 
         self.sol = now_sol
         self.iter = iteration
-        # self.time = 0
+        self.time = end_time - start_time
         self.num_call_nbl = num_call_nbl
+
+        if self.obj_func is null:
+            return 0
+        else:
+            self.sol_obj = self.obj_func(self.sol)
 
         return 0
 
 
+class FISTA_BACK:
+
+    # 初期設定
+    def __init__(self):
+        # 結果格納用
+        self.iter = null
+        self.sol = null
+        self.sol_obj = null
+        self.num_call_nbl = null
+        self.num_call_obj = null
+        self.lips = null
+        self.time = null
+        # 演算等に必要なデータ
+        self.obj_func = null
+        self.nbl_func = null
+        self.lips_init = null
+        self.x_init = null
+        self.conv_judge = null
+        self.back_para = null
+        self.output_iter = 1000
+
+    # 目的関数と勾配関数を設定(関数の引数はベクトルのみ)
+    def set_obj_func(self, obj_func):
+        self.obj_func = obj_func
+    def set_nbl_func(self, nbl_func):
+        self.nbl_func = nbl_func
+
+    # 計算に必要な各種設定をsetting
+    def set_lips_init(self, lips_init):
+        self.lips_init = lips_init
+    def set_output_iter(self, iter):
+        self.output_iter = iter
+    def set_conv_judge(self, conv):
+        self.conv_judge = conv
+    def set_back_para(self, para):
+        if para <= 1.0:
+            print("backtracking parameter is rather than 1.")
+            return 0
+        self.back_para = para
+    def set_x_init(self, x_init):
+        self.x_init = x_init
+
+    # FISTA with backtracking
+    def exect_FISTA_back(self):
+
+        is_Executed = 0
+
+        print("\n\n")
+
+        if self.x_init is null:
+            print("init sol is not set.")
+            is_Executed = -1
+        if self.obj_func is null:
+            print("objective function is not set.")
+            is_Executed = -1
+        if self.nbl_func is null:
+            print("nabla function is not set.")
+            is_Executed = -1
+        if self.lips_init is null:
+            print("initial lipschitz constant (>0) is not set.")
+            is_Executed = -1
+        if self.back_para is null:
+            print("backtracking parameter is not set.")
+            is_Executed = -1
+        if self.conv_judge is null:
+            print("convergence judgement (>0) is not set.")
+            is_Executed = -1
+
+        if is_Executed:
+            print("FISTA with backtracking cannot be executed.\n\n")
+            return -1
+
+        print('start Accel Gradient Method (FISTA with backtracking)!')
+
+        start_time = time.process_time()
+
+        # 初期設定（反復回数・勾配関数の呼び出し回数を初期化）
+        iteration = 0
+        num_call_obj = 0
+        num_call_nbl = 0
+        now_lips = self.lips_init
+
+        # 解の更新で使用する値
+        t = 1
+
+        # 初期解の設定
+        now_sol = self.x_init
+
+        temp_sol = now_sol
+
+        if len(now_sol) > 5:
+            print('iteration:', iteration, ' now_sol:', now_sol[:5])
+        else:
+            print('iteration:', iteration, ' now_sol:', now_sol)
+
+        while 1:
+
+            iteration += 1
+
+            # 勾配計算（一時的な解の）
+            temp_nbl = self.nbl_func(temp_sol)
+            num_call_nbl += 1
+
+            # backtracking
+            [now_lips, temp_call_obj, temp_call_nbl] = self.backtracking(temp_sol, now_lips)
+            num_call_obj += temp_call_obj
+            num_call_nbl += temp_call_nbl
+
+            # 暫定解の更新
+            prev_sol = now_sol
+            now_sol = temp_sol - temp_nbl/now_lips
+            temp_t = (1 + (1 + 4*t**2)**(1/2))/2
+            temp_sol = now_sol + ((t - 1)/temp_t) * (now_sol - prev_sol)
+            t = temp_t
+
+            # 収束判定の準備
+            now_nbl = self.nbl_func(now_sol)
+            num_call_nbl += 1
+            nbl_max = np.max(now_nbl)
+            nbl_min = np.min(now_nbl)
+
+            if iteration % self.output_iter == 0:
+                if len(now_sol) > 5:
+                    print('iteration:', iteration, ' now_sol:',
+                          now_sol[:5], ' max_nabla:', nbl_max, ' min_nabla:', nbl_min)
+                else:
+                    print('iteration:', iteration, ' now_sol:', now_sol,
+                          ' max_nabla:', nbl_max, ' min_nabla:', nbl_min)
+
+            # 収束判定
+            if nbl_max < self.conv_judge and -nbl_min < self.conv_judge:
+                break
+
+        end_time = time.process_time()
+
+        self.sol = now_sol
+        self.iter = iteration
+        self.time = end_time - start_time
+        self.num_call_nbl = num_call_nbl
+        self.num_call_obj = num_call_obj
+        self.sol_obj = self.obj_func(self.sol)
+
+        return 0
+
+
+
+    def backtracking(self, now_sol, now_lips):
+        
+        now_obj = self.obj_func(now_sol)
+        now_nbl = self.nbl_func(now_sol)
+        num_call_obj = 1
+        num_call_nbl = 1
+        now_nbl_nolm = np.dot(now_nbl, now_nbl)
+
+        iota = 0
+        while 1:
+
+            temp_sol = now_sol - (1/(self.back_para**iota * now_lips))*now_nbl
+
+            F = self.obj_func(temp_sol)
+            num_call_obj += 1
+            Q = now_obj - (1/(2*self.back_para**iota*now_lips))*now_nbl_nolm
+
+            if F <= Q:
+                break
+
+            iota += 1
+
+        return [self.back_para**iota*now_lips, num_call_obj, num_call_nbl]
 
 
 
@@ -95,15 +323,36 @@ if __name__ == '__main__':
 
     x_init = np.array([-20, 2, 3, 7, 8, 9, 0])
 
-    msa = AccelGradient()
-    msa.exect_FISTA(x_init, nbl_func, 200, 0.003, 5)
-
-    msa.sol_obj = obj_func(msa.sol)
+    fista = FISTA()
+    fista.set_x_init(x_init)
+    fista.set_obj_func(obj_func)
+    fista.set_nbl_func(nbl_func)
+    fista.set_lips(1000)
+    fista.set_conv_judge(0.03)
+    fista.exect_FISTA()
 
     print('\n\n')
 
+    print('sol: ', fista.sol)
+    print('sol_obj: ', fista.sol_obj)
+    print('iteration: ', fista.iter)
+    print('elapsed_time: ', fista.time)
+    print('num_call_nabla: ', fista.num_call_nbl)
 
-    print('sol: ', msa.sol)
-    print('sol_obj: ', msa.sol_obj)
-    print('iteration: ', msa.iter)
-    print('num_call_nabla: ', msa.num_call_nbl)
+    fista_back = FISTA_BACK()
+    fista_back.set_x_init(x_init)
+    fista_back.set_obj_func(obj_func)
+    fista_back.set_nbl_func(nbl_func)
+    fista_back.set_lips_init(0.01)
+    fista_back.set_back_para(1.1)
+    fista_back.set_conv_judge(0.03)
+    fista_back.exect_FISTA_back()
+
+    print('\n\n')
+
+    print('sol: ', fista_back.sol)
+    print('sol_obj: ', fista_back.sol_obj)
+    print('iteration: ', fista_back.iter)
+    print('elapsed_time: ', fista_back.time)
+    print('num_call_nabla: ', fista_back.num_call_nbl)
+    print('num_call_obj: ', fista_back.num_call_obj)
--- TNPandMS_lib/optimizationProgram/obj_class.py
+++
@@ -1,6 +0,0 @@
-# 目的関数のクラス
-class obj_class:
-    
-    def obj(self, x):
-
-        return 
