cd7214af608a1263513c3b90508642d724254ff5
TsubasaTakeda <80091069+TsubasaTakeda@users.noreply.github.com>
2023-01-05 23:55:53 +0900

    Frank-wolf法完成

--- TNPandMS_lib/optimizationProgram/linprog.py
+++ TNPandMS_lib/optimizationProgram/linprog.py
@@ -8,18 +8,19 @@ from sqlalchemy import null
 # min c x
 # s.t., B x <= b, 
 #       B_eq x = b_eq
-def linprog(c, B, b, B_eq = null, b_eq = null):
+def linprog(c, B = null, b = null, B_eq = null, b_eq = null):
 
-    if len(c) != B.shape[1]:
-        print('目的関数の係数ベクトルcと，制約条件の係数行列Bについて，変数の数が合っていません．')
-        print('c.shape: ', c.shape)
-        print('B.shape: ', B.shape)
-        return -1
-    elif B.shape[0] != len(b):
-        print('係数行列Bと，制約ベクトルbについて，制約条件の数が合っていません．')
-        print('B.shape: ', B.shape)
-        print('b.shpae: ', b.shape)
-        return -1
+    if B is not null:
+        if len(c) != B.shape[1]:
+            print('目的関数の係数ベクトルcと，制約条件の係数行列Bについて，変数の数が合っていません．')
+            print('c.shape: ', c.shape)
+            print('B.shape: ', B.shape)
+            return -1
+        elif B.shape[0] != len(b):
+            print('係数行列Bと，制約ベクトルbについて，制約条件の数が合っていません．')
+            print('B.shape: ', B.shape)
+            print('b.shpae: ', b.shape)
+            return -1
     elif B_eq is not null:
         if len(c) != B_eq.shape[1]:
             print('目的関数の係数ベクトルcと，制約条件の係数行列B_eqについて，変数の数が合っていません．')
@@ -44,8 +45,9 @@ def linprog(c, B, b, B_eq = null, b_eq = null):
     model.setObjective(gp.quicksum(c[i]*x[i] for i in range(len(c))), gp.GRB.MINIMIZE)
 
     # 制約条件の設定
-    for i in range(B.shape[0]):
-        model.addConstr(gp.quicksum(B[i, j] * x[j] for j in range(B.shape[1])) <= b[i])
+    if B is not null:
+        for i in range(B.shape[0]):
+            model.addConstr(gp.quicksum(B[i, j] * x[j] for j in range(B.shape[1])) <= b[i])
     if B_eq is not null:
         for i in range(B_eq.shape[0]):
             model.addConstr(gp.quicksum(B_eq[i, j] * x[j] for j in range(B_eq.shape[1])) == b_eq[i])
